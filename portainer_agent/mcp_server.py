1e2406b0aaf78c54bd4aeeb88a64ab363664c4ed
knucklessg1 <knucklessg1@gmail.com>
2026-05-29 04:12:11 -0500

    Bump version: 0.17.1 → 0.18.0

--- portainer_agent/mcp_server.py
+++ portainer_agent/mcp_server.py
@@ -32,7 +32,7 @@ from starlette.responses import JSONResponse
 
 from portainer_agent.auth import get_client
 
-__version__ = "0.17.1"
+__version__ = "0.18.0"
 
 logger = get_logger(name="portainer-agent")
 logger.setLevel(logging.INFO)
